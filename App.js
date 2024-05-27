ea7809f14d125c2662a2217bdd69f69c53a43546
Endurance27 <eoasabadu@st.ug.edu.gh>
2024-05-27 10:11:38 +0000

    completed files for the react native template

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.boldText}>Endurance offeibea</Text>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f8ff', // Change to your desired background color
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
+
